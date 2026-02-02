b34af28257b176555a89cd9fe829d41eb0a30021
Animesh Singh <animesh98388@gmail.com>
2026-02-03 02:09:54 +0530

    change astro assets dir to avoid github pages underscore issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
+import react from '@astrojs/react';
+
 export default defineConfig({
   site: 'https://blues1998.github.io',
   base: '/',
   output: 'static',
+
+  build: {
+    assets: 'astro',   // 🔴 IMPORTANT: no underscore
+  },
+
   integrations: [react()],
 });
