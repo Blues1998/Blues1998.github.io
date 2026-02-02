a87abe1bda4c64d103ec1eb3fa387c96e89ac18b
Animesh Singh <animesh98388@gmail.com>
2026-02-02 23:52:53 +0530

    change astro base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 export default defineConfig({
   site: 'https://blues1998.github.io',
-  base: '/new/',
+  base: '/',
   output: 'static',
   integrations: [react()],
 });
